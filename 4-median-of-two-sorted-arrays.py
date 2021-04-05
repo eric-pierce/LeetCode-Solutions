9198ce04177b61885843988726ac0dd38c1f09a3
Eric Pierce <eric@pierce.xyz>
2021-04-05 00:49:32 +0000

    Update 4-median-of-two-sorted-arrays.py

--- 4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays.py
@@ -1,6 +1,6 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        result_arr = []
+        result_arr = [0,0]
         j=0
         k=0
         j_spent = 0
@@ -18,13 +18,15 @@ class Solution:
             nums2.append(2001)
         for i in range(0,counter):
             if j_spent == 0 and (k_spent == 1 or (nums1[j] <= nums2[k])):
-                result_arr.append(nums1[j])
+                result_arr[-2] = result_arr[-1] 
+                result_arr[-1] = nums1[j]
                 if j + 1 < n1len:
                     j = j + 1
                 else:
                     j_spent = 1
             elif k_spent == 0 and (j_spent == 1 or (nums1[j] > nums2[k])):
-                result_arr.append(nums2[k])
+                result_arr[-2] = result_arr[-1] 
+                result_arr[-1] = nums2[k]
                 if k + 1 < n2len:
                     k = k + 1
                 else:
@@ -33,4 +35,3 @@ class Solution:
             return (result_arr[-1] + result_arr[-2]) / 2
         else:
             return result_arr[-1]
-            
